95172c438178f0fb22381337e6dfd37adccad0a8
Evan Simmons <esims89@gmail.com>
2013-07-10 11:32:14 -0700

    Fixed naming.

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
-  api.use('meteor-d3', 'client')
+  api.use('d3', 'client')
   api.add_files('rickshaw/rickshaw.js', 'client');
   api.add_files('rickshaw/rickshaw.css', 'client');
 });

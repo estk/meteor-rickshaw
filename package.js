8f12cd11decd576f874fdd7a1d89490c4ba2457e
Evan Simmons <esims89@gmail.com>
2013-07-10 10:04:48 -0700

    Required meteor-d3.

--- package.js
+++ package.js
@@ -3,6 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
+  api.use('meteor-d3', 'client')
   api.add_files('rickshaw/rickshaw.js', 'client');
   api.add_files('rickshaw/rickshaw.css', 'client');
 });
